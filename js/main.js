21203f620e15ab0ee79b03d99d3a81f0b161036a
kellymarieod <kellymarieod@gmail.com>
2016-05-04 20:52:02 -0400

    js test for home gallery carousel

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 $(document).ready( function() {
   console.log("I'M WORKING");
 });
+
+$("#carousel nav a").click(function(){
+  var ind = $(this).index("#carousel nav a");
+  console.log(ind);
+  $("carousel ul").animate({"margin-left": "-"+ind*650)};
+
+  )
+}
+)
